ac33b335806ec43009d950cbb93f4bab9740d3f1
Kirill Sytnyanskiy <nyan.kirill@gmail.com>
2021-09-21 13:51:09 +0300

    Картинка по середине

--- src/components/Quiz/QuizThanks.vue
+++ src/components/Quiz/QuizThanks.vue
@@ -2,7 +2,7 @@
   <div class="h-full">
     <h1 class="text-2xl text-white font-extrabold text-left">Спасибо!</h1>
     <p class="text-header mt-3 text-white font-extrabold leading-header">Наш менеджер скоро свяжется с вами</p>
-    <img src="./../../assets/banner.png" alt="Banner" class="absolute bottom-0">
+    <img src="./../../assets/banner.png" alt="Banner" class="absolute bottom-0 right-0">
   </div>
 </template>
 
